63cde5c5302e13c0d495b9fbfefe86d604af158e
Ricardo <ricardoado27@gmail.com>
2024-04-13 01:26:01 +0700

    Sementara Selesai. sudah post ke IG / linkedin. akan ad bugChanges kedepannya

--- authnya.py
+++ authnya.py
@@ -6,18 +6,22 @@ from fastapi_jwt import (
     JwtRefreshBearer
 )
 
+import os
+var = os.getenv('python_jwtfastapi_coffeeshop')
+print(var)
+
 # Secret Key ini harus dirahasiakan di .env
 # fastapi-jwt ga include dgn .env jd mw buat sndiri
 
 # Read access token from bearer header and cookie (bearer priority)
 access_security = JwtAccessBearerCookie(
-    secret_key="testingbuatsecretkeyjwt", #value defaultnya "secret_key". ini bs di modif rupany
+    secret_key=str(var), #value defaultnya "secret_key". ini bs di modif rupany
     auto_error=False,
     access_expires_delta=timedelta(hours=1)
 )
 
 # Read refresh token from bearer header only
 refresh_security = JwtRefreshBearer(
-    secret_key="testingbuatsecretkeyjwt", #value defaultnya "secret_key". ini bs di modif rupany
+    secret_key=str(var), #value defaultnya "secret_key". ini bs di modif rupany
     auto_error=True # automatically raise HTTPException: HTTP_401_UNAUTHORIZED
 )
